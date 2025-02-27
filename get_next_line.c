2459abaf7e03f4197b52c91dd5639ac54ab868c2
ricca@pinky <riccardofani@gmail.com>
2025-02-27 17:51:26 +0100

    refactor get_next_line to simplify buffer handling and improve readability

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rfani <rfani@student.42firenze.it>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 11:22:12 by rfani             #+#    #+#             */
-/*   Updated: 2025/02/24 18:01:49 by rfani            ###   ########.fr       */
+/*   Updated: 2025/02/27 17:47:05 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,16 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	static int	buffer_size;
 	static int	buffer_index;
-	t_list		*line_list;
+	char		*line_str;
 
 	if (BUFFER_SIZE <= 0 || fd < 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	line_list = NULL;
+	line_str = NULL;
 	if (buffer_size == 0)
 		buffer = gen_buffer(fd, &buffer_size);
 	if (!buffer)
 		return (NULL);
-	while (buffer_size > 0)
-	{
-		while (buffer_index < buffer_size && buffer[buffer_index] != '\n')
-			buffer_index++;
-		if (buffer[buffer_index] == '\n')
-		{
-			store_buffer(&line_list, buffer, &buffer_index, &buffer_size);
-			break ;
-		}
-		store_buffer(&line_list, buffer, &buffer_index, &buffer_size);
-	}
-	return (list_to_str(&line_list));
+	if (buffer_size > 0)
+		line_str = scan_buffer(fd, buffer, &buffer_index, &buffer_size);
+	return (line_str);
 }
