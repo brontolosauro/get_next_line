b67b6edffd9185fc14559c42ba063da8f5be0e3f
ricca@pinky <riccardofani@gmail.com>
2025-02-28 19:24:48 +0100

    core complete - implement store_item and lst_to_str functions for improved buffer handling in scan_buff

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rfani <rfani@student.42firenze.it>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 11:22:12 by rfani             #+#    #+#             */
-/*   Updated: 2025/02/28 12:08:11y rfani            ###   ########.fr       */
+/*   Updated: 2025/02/28 19:10:16 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 char	*get_next_line(int fd);
 char	*gen_buff(int fd, int *buff_size);
 char	*scan_buff(int fd, char **buffer, int *buff_index, int *buff_size);
+int		store_item(t_list **line_lst, char buff_item);
+char	*lst_to_str(t_list **line_lst);
 
 char	*get_next_line(int fd)
 {
@@ -57,10 +59,15 @@ char	*gen_buff(int fd, int *buff_size)
 char	*scan_buff(int fd, char **buffer, int *buff_index, int *buff_size)
 {
 	t_list	*line_lst;
+	int		storage_error;
+	char	*line_str;
 
-	while (*buffer[*buff_index] != '\n' && *buff_index < *buff_size)
+	line_lst = NULL;
+	while (*buffer[*buff_index] != '\n')
 	{
-		store_element(&line_lst, *buffer[*buff_index]);
+		storage_error = store_item(&line_lst, *buffer[*buff_index]);
+		if (storage_error)
+			return (NULL);
 		if (*buff_index == *buff_size - 1)
 		{
 			*buff_size = read(fd, *buffer, BUFFER_SIZE);
@@ -72,4 +79,51 @@ char	*scan_buff(int fd, char **buffer, int *buff_index, int *buff_size)
 			return (NULL);
 		(*buff_index)++;
 	}
+	line_str = lst_to_str(&line_lst);
+	return (line_str);
+}
+
+int	store_item(t_list **line_lst, char buff_item)
+{
+	char	*item;
+	t_list	*tail;
+
+	item = (char *)malloc(sizeof(char));
+	if (!item)
+		return (1);
+	if (!*line_lst)
+	{
+		*line_lst = ft_lstnew(item);
+		if (!*line_lst)
+			return (1);
+	}
+	else
+	{
+		tail = ft_lstnew(item);
+		if (!tail)
+			return (1);
+		ft_lstadd_back(line_lst, tail);
+		return (0);
+	}
+}
+
+char	*lst_to_str(t_list **line_lst)
+{
+	char	*line_str;
+	t_list	*temp;
+	int		i;
+
+	line_str = (char *)malloc(sizeof(char) * ft_lstsize(*line_lst));
+	if (!line_str)
+		return (NULL);
+	i = 0;
+	temp = *line_lst;
+	while (temp)
+	{
+		line_str[i] = *((char *)temp->content);
+		temp = temp->next;
+		i++;
+	}
+	ft_lstclear(line_lst, free);
+	return (line_str);
 }
